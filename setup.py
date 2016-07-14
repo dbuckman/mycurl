250eb77ea8e3feb078df1051abcb29b7fd787338
Drew Buckman <dbuckma@us.ibm.com>
2016-07-14 09:51:56 -0400

    Updating setup.py to suport pypi

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'mycurl',
   packages = ['mycurl'],
-  version = '0.1.2',
+  version = '0.1.3',
   description = 'A pycurl wrapper that tries to make pycurl work like requests',
   author = 'Drew Buckman',
   author_email = 'drew.buckman@gmail.com',
   url = 'https://github.com/dbuckman/mycurl',
-  download_url = 'https://github.com/dbuckman/mycurl/tarball/0.1.2',
+  download_url = 'https://github.com/dbuckman/mycurl/tarball/0.1.3',
   keywords = ['MycURL', 'cURL', 'HTTP'],
   classifiers = [],
   INSTALL_REQUIRES = [
